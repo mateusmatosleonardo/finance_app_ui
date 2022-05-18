00c4041ff3ca3434722df9fe6528003231ad8cb8
Mateus Leonardo <m4teusmatos@gmail.com>
2022-05-18 20:28:25 -0300

    finalizing signIn

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.inputArea} />
-      <View style={[styles.inputArea, {marginTop: 20}]} />
+      <View style={styles.contentInput}>
+        <Text style={styles.titleInput}>Nome</Text>
+        <View style={styles.inputArea}>
+          <TextInput style={{paddingLeft: 8, color: '#444'}} maxLength={20} />
+        </View>
+      </View>
+      <View style={styles.contentInput}>
+        <Text style={styles.titleInput}>E-mail</Text>
+        <View style={styles.inputArea}>
+          <TextInput
+            style={{paddingLeft: 8, color: '#444'}}
+            keyboardType={'email-address'}
+          />
+        </View>
+      </View>
+      <View style={styles.contentInput}>
+        <Text style={styles.titleInput}>Password</Text>
+        <View style={styles.inputArea}>
+          <TextInput style={{paddingLeft: 8, color: '#444'}} secureTextEntry />
+        </View>
+      </View>
+      <TouchableOpacity style={styles.buttonSignIn} activeOpacity={0.8}>
+        <Text style={{color: '#fff', marginRight: 15, fontSize: 16}}>
+          Sign-In
+        </Text>
+        <Icon name="sign-in" color="#fff" size={22} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -24,6 +56,25 @@ const styles = StyleSheet.create({
     height: 45,
     borderRadius: 8,
     backgroundColor: '#dedede',
+  },
+  contentInput: {
+    width: '100%',
+    marginBottom: 10,
+  },
+  titleInput: {
+    fontSize: 16,
+    color: '#444',
+    marginBottom: 6,
+  },
+  buttonSignIn: {
+    width: '80%',
+    height: 45,
+    marginTop: 15,
+    borderRadius: 8,
+    backgroundColor: '#FF941A',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
